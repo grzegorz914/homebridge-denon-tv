03aeb3fd7d5f60351830ddf75d9f14c7111b3db0
Grzegorz <grzegorz914@icloud.com>
2020-04-23 09:58:59 +0200

    bump 1.2.63

--- index.js
+++ index.js
@@ -434,6 +434,8 @@ class denonTvDevice {
 								me.log('Device: %s, get current Input successful: %s', me.host, inputReference);
 								me.currentInputReference = inputReference;
 								callback(null, i);
+							} else {
+								callback(null, 0);
 							}
 						}
 					}
