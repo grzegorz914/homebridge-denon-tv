606f3283bcfbaf8e63b94609f544bcd47b0a700c
Grzegorz <grzegorz914@icloud.com>
2020-05-16 11:08:54 +0200

    bump 2.1.1

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ class denonTvDevice {
 				let powerState = (result.item.Power[0].value[0] == "ON");
 				if (me.televisionService && (powerState !== me.currentPowerState)) {
 					me.televisionService.updateCharacteristic(Characteristic.Active, powerState);
-					me.log("Device: %s %s %s, get current Power state successful: %s", me.host, me.name, me.zoneName, powerState ? "ON" : "OFF");
+					me.log("Device: %s %s %s, get current Power state successful: %s", me.host, me.name, me.zoneName, powerState ? "ON" : (me.masterPowerControl ? "STANDBY" : "OFF"));
 					me.currentPowerState = powerState;
 				}
 
@@ -408,7 +408,7 @@ class denonTvDevice {
 		if (state !== me.currentPowerState) {
 			let newState = me.masterPowerControl ? (state ? "PWON" : "PWSTANDBY") : [(state ? "ZMON" : "ZMOFF"), (state ? "Z2ON" : "Z2OFF"), (state ? "Z3ON" : "Z3OFF")][me.zoneControl];
 			axios.get(me.url + "/goform/formiPhoneAppDirect.xml?" + newState).then(response => {
-				me.log("Device: %s %s %s, set new Power state successful: %s", me.host, me.name, me.zoneName, state ? "ON" : "OFF");
+				me.log("Device: %s %s %s, set new Power state successful: %s", me.host, me.name, me.zoneName, state ? "ON" : (me.masterPowerControl ? "STANDBY" : "OFF"));
 				callback(null);
 			}).catch(error => {
 				if (error) {
