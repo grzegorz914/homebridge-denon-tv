fbea12425cb4b5aa994ed1ea5ac4bb684668b1ba
Grzegorz <grzegorz914@icloud.com>
2022-12-18 10:34:58 +0100

    - fix buttons and switch services

--- index.js
+++ index.js
@@ -870,10 +870,6 @@ class denonTvDevice {
 							try {
 								const setSwitchInput = (state && this.power) ? await this.denon.send(CONSTANS.ApiUrls.iPhoneDirect + inputSwitchRef) : false;
 								const logInfo = this.disableLogInfo ? false : this.log('Device: %s %s, set new Input successful, name: %s, reference: %s', this.host, accessoryName, inputSwitchName, inputSwitchReference);
-
-								setTimeout(() => {
-									const setChar = (state && this.power) ? switchService.updateCharacteristic(Characteristic.On, false) : false;
-								}, 300)
 							} catch (error) {
 								this.log.error('Device: %s %s, can not set new Input. Might be due to a wrong settings in config, error: %s.', this.host, accessoryName, error);
 							};
