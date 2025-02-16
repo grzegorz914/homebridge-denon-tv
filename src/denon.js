b207907a98d194ee967a4ef076c2087dd94aa3ab
Grzegorz <grzegorz914@icloud.com>
2025-02-16 14:09:25 +0100

    cleanup

--- src/denon.js
+++ src/denon.js
@@ -491,7 +491,6 @@ class Denon extends EventEmitter {
         try {
             const path = `${ApiUrls.iPhoneDirect}${command}`;
             await this.axiosInstance(path);
-            this.emit('warn', `denon send path: ${path}`);
             return true;
         } catch (error) {
             throw new Error(`Send data error: ${error}`);
